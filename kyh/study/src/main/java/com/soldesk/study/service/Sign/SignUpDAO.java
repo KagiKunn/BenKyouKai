8a83ddb9a4baf2cb6d67ed50f5ccfc5a5a9cbc71
KONG1034 <kyh93054@naver.com>
2024-12-20 21:42:16 +0900

    kyh - 2주차 과제 완성

---
+++ kyh/study/src/main/java/com/soldesk/study/service/Sign/SignUpDAO.java
@@ -0,0 +1,24 @@
+package com.soldesk.study.service.Sign;
+
+import com.soldesk.study.dto.UserDTO;
+import com.soldesk.study.mapper.Sign.SignUpMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SignUpDAO implements SignUpMapper{
+    @Autowired
+    private SignUpMapper signUpMapper;
+
+    @Override
+    public int insertUser(UserDTO userDto) {
+        return signUpMapper.insertUser(userDto);
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers() {
+        return signUpMapper.getAllUsers();
+    }
+}
---
+++ kyh/study/src/main/resources/static/js/index.js
@@ -0,0 +1,4 @@
+const goSignUp = () => {
+    console.log("js 들어옴")
+    location.href = "/sign/goSignUp";
+}
