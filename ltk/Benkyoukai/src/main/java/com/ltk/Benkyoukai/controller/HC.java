87516e4b01b9efb25fa422e03a82070e9edb4df0
STELLA\stell <leeteakoni@gmail.com>
2024-12-26 02:05:06 +0900

    failed mission 3rd commit

--- ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/controller/HC.java
+++ ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/controller/HC.java
@@ -1,14 +1,31 @@
 package com.ltk.Benkyoukai.controller;
 
+import com.ltk.Benkyoukai.dto.BenkyoukaiVO;
+import com.ltk.Benkyoukai.service.BenkyoukaiService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class HC {
+
     @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("name", "Gradel User");
-        return "index";
+    public String main() {
+        return "main";
+    }
+
+    @PostMapping("/users/register")
+    public String registerUser(BenkyoukaiVO benkyoukaiVO) {
+        BenkyoukaiService.registerUser(benkyoukaiVO);
+        return "redirect:/show";
+    }
+
+    @GetMapping("/list")
+    public String getUsers(Model model) {
+        model.addAttribute("users", BenkyoukaiService.getAllUsers());
+        return "userlist";
     }
 }
