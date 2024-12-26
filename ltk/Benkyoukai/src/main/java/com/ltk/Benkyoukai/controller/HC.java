0bcb657f830e370422257b8c736297f673c8153c
STELLA\stell <leeteakoni@gmail.com>
2024-12-26 17:17:00 +0900

    [feat] select insert function normalized

--- ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/controller/HC.java
+++ ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/controller/HC.java
@@ -2,15 +2,20 @@ package com.ltk.Benkyoukai.controller;
 
 import com.ltk.Benkyoukai.dto.BenkyoukaiVO;
 import com.ltk.Benkyoukai.service.BenkyoukaiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
 public class HC {
+
+    private final BenkyoukaiService benkyoukaiService;
+
+    public HC(BenkyoukaiService benkyoukaiService) {
+        this.benkyoukaiService = benkyoukaiService;
+    }
 
     @GetMapping("/")
     public String main() {
@@ -18,14 +23,16 @@ public class HC {
     }
 
     @PostMapping("/users/register")
-    public String registerUser(BenkyoukaiVO benkyoukaiVO) {
-        BenkyoukaiService.registerUser(benkyoukaiVO);
-        return "redirect:/show";
-    }
+    public String registerUser(BenkyoukaiVO benkyoukaiVO, Model model) {
+        benkyoukaiService.registerUser(benkyoukaiVO);
 
-    @GetMapping("/list")
-    public String getUsers(Model model) {
-        model.addAttribute("users", BenkyoukaiService.getAllUsers());
+        model.addAttribute("users", benkyoukaiService.getAllUsers());
         return "userlist";
     }
+
+//    @GetMapping("/show")
+//    public String getUsers(Model model) {
+//        model.addAttribute("users", benkyoukaiService.getAllUsers());
+//        return "userlist";
+//    }
 }
