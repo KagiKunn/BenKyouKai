9c4955b0a0d336a8080456e8ca53fbde0ef2ff2e
STELLA\stell <leeteakoni@gmail.com>
2025-01-17 00:45:11 +0900

    update & password complete

--- ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/service/BenkyoukaiService.java
+++ ltk/Benkyoukai/src/main/java/com/ltk/Benkyoukai/service/BenkyoukaiService.java
@@ -3,6 +3,7 @@ package com.ltk.Benkyoukai.service;
 import com.ltk.Benkyoukai.dto.BenkyoukaiVO;
 import com.ltk.Benkyoukai.mapper.BenkyoukaiMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +12,14 @@ import java.util.List;
 public class BenkyoukaiService {
 
    @Autowired BenkyoukaiMapper benkyoukaiMapper;
+
+   @Autowired
+   PasswordEncoder passwordEncoder;
+
+//   public void registerUser(String id, String rawPassword) {
+//       String encodePassword = passwordEncoder.encode(rawPassword);
+//       System.out.println("암호화된 비번: " + encodePassword);
+//   }
 
    public boolean isIdDuplicate(String id) {
        System.out.println( "중복확인 : " + benkyoukaiMapper.checkIdDuplicate(id));
@@ -24,19 +33,33 @@ public class BenkyoukaiService {
        return benkyoukaiMapper.getUserByID(id);
    }
 
+   public void updateUser(BenkyoukaiVO benkyoukaiVO) {
+       benkyoukaiMapper.updateUser(benkyoukaiVO);
+   }
+
     public void registerUser(BenkyoukaiVO benkyoukaiVO) {
-        benkyoukaiMapper.insertUsers(benkyoukaiVO);
+
+       String encodePassword = passwordEncoder.encode(benkyoukaiVO.getPassword());
+       System.out.println("암호화된 비번: " + encodePassword);
+
+       benkyoukaiVO.setPassword(encodePassword);
+
+       benkyoukaiMapper.insertUsers(benkyoukaiVO);
+
     }
 
     public List<BenkyoukaiVO> getAllUsers() {
         return benkyoukaiMapper.getAllUsers();
+
     }
 
     public BenkyoukaiVO getUser(String id, String password) {
+
         return benkyoukaiMapper.getUser(id, password);
     }
 
     public void deleteUser(String id) {
+
        benkyoukaiMapper.deleteUser(id);
     }
 }
